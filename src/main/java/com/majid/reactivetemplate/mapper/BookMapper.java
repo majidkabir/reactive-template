140f3a6db57ae4723dffbd12429d1a6faf2d7a78
Majid Ghafouri <majid.ghafouri@maersk.com>
2023-08-18 15:46:46 +0200

    Not mapping the id from DTO to Entity

--- src/main/java/com/majid/reactivetemplate/mapper/BookMapper.java
+++ src/main/java/com/majid/reactivetemplate/mapper/BookMapper.java
@@ -3,9 +3,12 @@ package com.majid.reactivetemplate.mapper;
 import com.majid.reactivetemplate.dto.BookDto;
 import com.majid.reactivetemplate.model.Book;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper
 public interface BookMapper {
     BookDto toDto(Book book);
+
+    @Mapping(target = "id", ignore = true)
     Book fromDto(BookDto dto);
 }
